4635a6b9594809d238b5d63e54069cd5bfbb64ec
Cameron Silverman <117317913+CameronSilverman@users.noreply.github.com>
2024-10-26 14:53:40 -0400

    added nicer buttons to landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -8,15 +8,18 @@ export default function Home() {
     <main>
       <div className="background-div-style">
         <div className="link-div-style">
-          <div>
-            <Link href="/project_creation">Project Creation Page</Link>
-          </div>
-          <div>
-            <Link href="/sign_up_page">Signup Page</Link>
-          </div>
-          <div>
-            <Link href="/sign_in_page">Log In</Link>
-          </div>
+          
+            <Link href="/sign_in_page">
+              <button className = "bg-orange-500 text-white px-4 py-2 rounded-lg hover:bg-orange-600 transition-colors">Log In</button>
+            </Link>
+            <Link href="/project_creation">
+              <button className = "bg-white text-black px-4 py-2 rounded-lg hover:bg-neutral-300 transition-colors">Project Creation Page</button>
+            </Link>
+          
+            <Link href="/sign_up_page">
+            <button className = "bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition-colors">Signup</button>
+            </Link>
+          
         </div>
       </div>
     </main>
