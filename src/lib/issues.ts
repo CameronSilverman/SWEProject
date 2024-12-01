0f827f70cf9ac0514dfa7a27810d72663bd5b2ca
Sniped <colin@colinmcc.me>
2024-11-30 19:43:01 -0500

    move the use server directive to the top

--- src/lib/issues.ts
+++ src/lib/issues.ts
@@ -1,8 +1,8 @@
+'use server'
+
 import { IssueCreateFormData } from '@/components/form/issue/issue-create-form';
 import { db } from './db';
 import { projectTasks } from './db/schema';
-
-'use server'
 
 export async function createIssueWithData(data: IssueCreateFormData) {
 	const res = await db.insert(projectTasks).values({
